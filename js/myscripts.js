54ceb915c5f37c69e336e8e3e461f3a46d2c2e70
Maka Basilio <vinicius06designer@gmail.com>
2017-09-13 22:13:54 +1000

    Section Heroes logos in place and buttons working

--- js/myscripts.js
+++ js/myscripts.js
@@ -16,12 +16,171 @@ $(document).ready(function(){
 	    scrollBy(0, -offset);
 
 	    
-	});
+	}); // scroll spy offset ends
 
 
 
-/ hero selection /
+$("li.greenlantern-btn").click (function () {
 
+
+	$("#starwars").fadeOut(200);
+	$("#flash").fadeOut(200);
+	$("#batman").fadeOut(200);
+	$("#captainamerica").fadeOut(200);
+	$("#spiderman").fadeOut(200);
+	$("#pirate").fadeOut(200);
+	$("#superman").fadeOut(200);
+	$("#ninja").fadeOut(200);
+
+
+	$("#greenlantern").fadeIn(600);
+
+
+}); // green lantern ends
+
+
+$("li.starwars-btn").click (function () {
+
+
+	$("#flash").fadeOut(200);
+	$("#batman").fadeOut(200);
+	$("#captainamerica").fadeOut(200);
+	$("#spiderman").fadeOut(200);
+	$("#pirate").fadeOut(200);
+	$("#superman").fadeOut(200);
+	$("#ninja").fadeOut(200);
+	$("#greenlantern").fadeOut(200);
+
+	$("#starwars").fadeIn(600);
+
+
+}); // starwars ends
+
+
+$("li.flash-btn").click (function () {
+
+
+	$("#starwars").fadeOut(200);
+	$("#batman").fadeOut(200);
+	$("#captainamerica").fadeOut(200);
+	$("#spiderman").fadeOut(200);
+	$("#pirate").fadeOut(200);
+	$("#superman").fadeOut(200);
+	$("#ninja").fadeOut(200);
+	$("#greenlantern").fadeOut(200);
+
+	$("#flash").fadeIn(600);
+
+
+}); // flash ends
+
+
+$("li.batman-btn").click (function () {
+
+
+	$("#starwars").fadeOut(200);
+	$("#flash").fadeOut(200);
+	$("#captainamerica").fadeOut(200);
+	$("#spiderman").fadeOut(200);
+	$("#pirate").fadeOut(200);
+	$("#superman").fadeOut(200);
+	$("#ninja").fadeOut(200);
+	$("#greenlantern").fadeOut(200);
+
+	$("#batman").fadeIn(600);
+
+
+}); // batman ends
+
+
+$("li.captainamerica-btn").click (function () {
+
+
+	$("#starwars").fadeOut(200);
+	$("#flash").fadeOut(200);
+	$("#batman").fadeOut(200);
+	$("#spiderman").fadeOut(200);
+	$("#pirate").fadeOut(200);
+	$("#superman").fadeOut(200);
+	$("#ninja").fadeOut(200);
+	$("#greenlantern").fadeOut(200);
+
+	$("#captainamerica").fadeIn(600);
+
+
+}); // captainamerica ends
+
+
+$("li.spiderman-btn").click (function () {
+
+
+	$("#starwars").fadeOut(200);
+	$("#flash").fadeOut(200);
+	$("#batman").fadeOut(200);
+	$("#captainamerica").fadeOut(200);
+	$("#pirate").fadeOut(200);
+	$("#superman").fadeOut(200);
+	$("#ninja").fadeOut(200);
+	$("#greenlantern").fadeOut(200);
+
+	$("#spiderman").fadeIn(600);
+
+
+}); // spiderman ends
+
+
+$("li.pirate-btn").click (function () {
+
+
+	$("#starwars").fadeOut(200);
+	$("#flash").fadeOut(200);
+	$("#batman").fadeOut(200);
+	$("#captainamerica").fadeOut(200);
+	$("#spiderman").fadeOut(200);
+	$("#superman").fadeOut(200);
+	$("#ninja").fadeOut(200);
+	$("#greenlantern").fadeOut(200);
+
+	$("#pirate").fadeIn(600);
+
+
+}); // pirate ends
+
+
+$("li.superman-btn").click (function () {
+
+
+	$("#starwars").fadeOut(200);
+	$("#flash").fadeOut(200);
+	$("#batman").fadeOut(200);
+	$("#captainamerica").fadeOut(200);
+	$("#spiderman").fadeOut(200);
+	$("#pirate").fadeOut(200);
+	$("#ninja").fadeOut(200);
+	$("#greenlantern").fadeOut(200);
+
+	$("#superman").fadeIn(600);
+
+
+}); // superman ends
+
+$("li.ninja-btn").click (function () {
+
+
+	$("#starwars").fadeOut(200);
+	$("#flash").fadeOut(200);
+	$("#batman").fadeOut(200);
+	$("#captainamerica").fadeOut(200);
+	$("#spiderman").fadeOut(200);
+	$("#pirate").fadeOut(200);
+	$("#superman").fadeOut(200);
+	$("#ninja").fadeOut(200);
+	$("#greenlantern").fadeOut(200);
+
+	$("#ninja").fadeIn(600);
+
+
+}); // ninja ends
 
 
 	  
@@ -30,4 +189,4 @@ $(document).ready(function(){
 	 document.querySelector(".card-flip").classList.toggle("flip");
 
 
-});
+}); // document ends
